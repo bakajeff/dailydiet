7850c8809a72137b33956ad9d23f625ad22898c3
jeffte costa <jefftewf17@outlook.com>
2023-02-03 23:45:54 -0300

    refactor: remove screen folder

--- App.tsx
+++ App.tsx
@@ -1,13 +1,12 @@
+import { ActivityIndicator, Text } from "react-native";
 import { ThemeProvider } from "styled-components/native";
 import {
 	useFonts,
 	NunitoSans_400Regular,
 	NunitoSans_700Bold,
 } from "@expo-google-fonts/nunito-sans";
-import { Home } from "./src/screen/Home";
 
 import theme from "./src/theme";
-import { ActivityIndicator } from "react-native";
 
 export default function App() {
 	const [fontsLoaded] = useFonts({
@@ -18,7 +17,7 @@ export default function App() {
 	return (
 		<ThemeProvider theme={theme}>
 			{fontsLoaded ? (
-				<Home />
+				<Text>Hello world</Text>
 			) : (
 				<ActivityIndicator
 					style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
