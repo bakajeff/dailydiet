b070cf191c1465ed3e6706d675eac3837c7d7b4a
jeffte costa <jefftewf17@outlook.com>
2023-02-12 15:18:24 -0300

    feat: add MeadHeader style

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,13 @@
 import { HomeHeader } from "@components/HomeHeader";
 import { PercentCard } from "@components/PercentCard";
-import { Container } from "./styles";
+import { Container, MealHeader } from "./styles";
 
 export function Home() {
 	return (
 		<Container>
 			<HomeHeader />
 			<PercentCard type="PRIMARY" percent="90,86" />
+			<MealHeader>Refeições</MealHeader>
 		</Container>
 	);
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,5 +1,14 @@
-import styled from "styled-components/native";
+import styled, {css} from "styled-components/native";
 
 export const Container = styled.View`
 	padding: 0 24px;
+`;
+
+export const MealHeader = styled.Text`
+	${({theme}) => css`
+		font-size: ${theme.FONT_SIZE.MD}px;
+		font-family: ${theme.FONT_FAMILY.REGULAR};
+		color: ${theme.COLORS.GRAY_100};
+	`};
+	margin-top: 40px;
 `;
