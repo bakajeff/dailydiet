34f18592a408621181f843e00dcc0de6d87560e7
jeffte costa <jefftewf17@outlook.com>
2023-02-18 19:54:54 -0300

    feat: add Details screen

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import {
 } from "@expo-google-fonts/nunito-sans";
 
 import theme from "./src/theme";
-import { Home } from "./src/screens/Home";
+import { Details } from "./src/screens/Details";
 
 export default function App() {
 	const [fontsLoaded] = useFonts({
@@ -18,7 +18,7 @@ export default function App() {
 	return (
 		<ThemeProvider theme={theme}>
 			{fontsLoaded ? (
-				<Home />
+				<Details />
 			) : (
 				<ActivityIndicator
 					style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
