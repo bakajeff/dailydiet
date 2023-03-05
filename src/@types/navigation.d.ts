7511f4b34dffa9fc9246b84ff8974d1b383effe2
jeffte costa <jefftewf17@outlook.com>
2023-03-05 19:23:00 -0300

    feat: add type definition to routes

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,11 @@
+export declare global {
+	namespace ReactNavigation {
+		interface RootParamList {
+			home: undefined;
+			meal: undefined;
+			new: undefined;
+			feedback: undefined;
+			statistics: undefined;
+		}
+	}
+}
