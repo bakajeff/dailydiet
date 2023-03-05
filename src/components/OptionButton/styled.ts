bf2bbf4383e706ba1639188c2a4e84bc98741dc1
jeffte costa <jefftewf17@outlook.com>
2023-03-05 20:33:57 -0300

    refactor: add border to prevent layout bugs

--- src/components/OptionButton/styled.ts
+++ src/components/OptionButton/styled.ts
@@ -4,13 +4,13 @@ import styled, { css } from "styled-components/native";
 export type IndicatorStyleProps = "PRIMARY" | "SECONDARY";
 
 type IndicatorProps = {
-	type: IndicatorStyleProps
-}
+	type: IndicatorStyleProps;
+};
 
 type ContainerProps = {
-  type: IndicatorStyleProps;
+	type: IndicatorStyleProps;
 	active: boolean;
-}
+};
 
 export const Container = styled(Pressable)<ContainerProps>`
 	flex-direction: row;
@@ -21,18 +21,21 @@ export const Container = styled(Pressable)<ContainerProps>`
 	align-items: center;
 	justify-content: center;
 
-	background-color: ${({theme}) => theme.COLORS.GRAY_600};
+	background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+	border: 1px solid ${({ theme }) => theme.COLORS.GRAY_600};
 
-	${({type, active, theme}) => 
-		(active && type === "PRIMARY")
-			&& css`
+	${({ type, active, theme }) =>
+		active &&
+		type === "PRIMARY" &&
+		css`
 				background-color:  ${theme.COLORS.GREEN_LIGHT};
 				border: 1px solid ${theme.COLORS.GREEN_DARK};
 			`};
 
-	${({type, active, theme}) => 
-		(active && type === "SECONDARY")
-			&& css`
+	${({ type, active, theme }) =>
+		active &&
+		type === "SECONDARY" &&
+		css`
 				background-color: ${theme.COLORS.RED_LIGHT};
 				border: 1px solid ${theme.COLORS.RED_DARK};
 			`};
@@ -49,13 +52,12 @@ export const Indicator = styled.View<IndicatorProps>`
 
 	border-radius: 9999px;
 	
-	background-color: ${({theme, type}) => type === "PRIMARY" 
-	? theme.COLORS.GREEN_DARK
-	: theme.COLORS.RED_DARK }
+	background-color: ${({ theme, type }) =>
+		type === "PRIMARY" ? theme.COLORS.GREEN_DARK : theme.COLORS.RED_DARK}
 `;
 
 export const Title = styled.Text`
-	${({theme}) => css`
+	${({ theme }) => css`
 		font-size: ${theme.FONT_SIZE.SM}px;
 		font-family: ${theme.FONT_FAMILY.BOLD};
 	`};
